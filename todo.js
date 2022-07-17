66943daa92199f997a30bf5deffb0bb43bdf1d30
pravda23 <pravda23@gmail.com>
2022-07-17 13:55:49 +0400

    rejects new tasks if they exist (console only)

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
-const arr = [];
+const arr = ["1", "2", "3"];
 
 // selectors
+
 const input = document.querySelector(".input");
 const inputBtn = document.querySelector(".inputBtn");
 const list = document.querySelector(".list");
@@ -11,40 +12,59 @@ inputBtn.addEventListener("click", addEntry);
 // add single entry
 function addEntry(e) {
   e.preventDefault();
-
-  // validate text entry
   input.placeholder = "";
+
+  // IS INPUT FIELD BLANK?
+  // TRUE
   if (input.value === "") {
     input.placeholder = "Please enter something.";
+
+    // FALSE
   } else {
-    // create single entry
-    const singleEntry = document.createElement("div");
-    singleEntry.classList.add("singleEntry");
+    // IS NEW TASK A UNIQUE ENTRY?
 
-    // create li
-    const entryText = document.createElement("li");
-    entryText.innerText = input.value;
-    entryText.classList.add("entryText");
-    singleEntry.appendChild(entryText);
-    arr.push(input.value);
-    console.log(arr);
+    // assume new task is unique until scanning arr proves otherwise
 
-    // create checked button
-    const checkedButton = document.createElement("button");
-    checkedButton.classList.add("btn-checked");
-    checkedButton.innerHTML = '<i class="fas fa-check"></i>';
-    singleEntry.appendChild(checkedButton);
+    const isUnique = (currentTask) => currentTask !== input.value;
 
-    // create trash button
-    const trashButton = document.createElement("button");
-    trashButton.classList.add("btn-trash");
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    singleEntry.appendChild(trashButton);
+    const uniqueEntry = arr.every(isUnique);
 
-    // append singleEntry to list container, clear form
-    list.appendChild(singleEntry);
-    input.value = "";
+    // FALSE
+    if (uniqueEntry === false) {
+      //   input.placeholder = "You've already added that task.";
+      console.log("You've already added that task.");
+      input.placeholder = "";
+    } else {
+      // TRUE
+      // create single entry
+      const singleEntry = document.createElement("div");
+      singleEntry.classList.add("singleEntry");
+
+      // create li
+      const entryText = document.createElement("li");
+      entryText.innerText = input.value;
+      entryText.classList.add("entryText");
+      singleEntry.appendChild(entryText);
+      arr.push(input.value);
+
+      // create checked button
+      const checkedButton = document.createElement("button");
+      checkedButton.classList.add("btn-checked");
+      checkedButton.innerHTML = '<i class="fas fa-check"></i>';
+      singleEntry.appendChild(checkedButton);
+
+      // create trash button
+      const trashButton = document.createElement("button");
+      trashButton.classList.add("btn-trash");
+      trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+      singleEntry.appendChild(trashButton);
+
+      // append singleEntry to list container, clear form
+      list.appendChild(singleEntry);
+      input.value = "";
+    }
   }
+  console.log(arr);
 }
 // mark as checked and delete
 
@@ -59,6 +79,5 @@ addEventListener("click", (e) => {
     e.target.parentElement.remove();
     const indexOfItemToRemove = arr.indexOf(e.target.parentElement.textContent);
     arr.splice(indexOfItemToRemove, 1);
-    console.log(arr);
   }
 });
